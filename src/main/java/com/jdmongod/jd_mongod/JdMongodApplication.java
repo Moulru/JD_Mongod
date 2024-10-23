3e05e1af48249f5b030538ef60e0c62ec9aeaacd
JH <qkrwnsgh234@gmail.com>
2024-10-23 16:16:45 +0900

    first commit.

---
+++ src/main/java/com/jdmongod/jd_mongod/JdMongodApplication.java
@@ -0,0 +1,13 @@
+package com.jdmongod.jd_mongod;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class JdMongodApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(JdMongodApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/jdmongod/jd_mongod/JdMongodApplicationTests.java
@@ -0,0 +1,13 @@
+package com.jdmongod.jd_mongod;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class JdMongodApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
