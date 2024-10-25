53ef528fa9066e3ef8700066d287a66cc072dde8
startDev01 <startDev01@gmail.com>
2024-10-25 15:18:20 +0900

    메인 컨트롤러 수정 프론트단 폴더 정렬

--- src/main/java/com/jdmongod/jd_mongod/controller/JDMongodMainController.java
+++ src/main/java/com/jdmongod/jd_mongod/controller/JDMongodMainController.java
@@ -1,13 +1,15 @@
 package com.jdmongod.jd_mongod.controller;
 
 import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
 
 // JDMongod 메인 컨트롤러
 @Controller
 public class JDMongodMainController {
 
   // 임시 컨트롤러
+  @GetMapping("/noticeBoard")
   public String firstController() {
-    return "";
+    return "noticeBoard";
   }
 }
